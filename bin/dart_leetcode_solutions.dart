9285926339d232e4fc324f72aa5105e4c5206ff9
ADILAYOUB <mintofree840@gmail.com>
2023-05-21 12:16:37 +0530

    initial commit

---
+++ bin/dart_leetcode_solutions.dart
@@ -0,0 +1,5 @@
+import 'package:dart_leetcode_solutions/dart_leetcode_solutions.dart' as dart_leetcode_solutions;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_leetcode_solutions.calculate()}!');
+}
